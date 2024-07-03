3b8e2dec30548c200eeccbbcca826e0622b9c677
Eduardo Consalvo <educon001@gmail.com>
2024-07-03 01:23:38 -0400

    Delete routes.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,11 +1,10 @@
-from . import ingredients, dishes, orders, routes
+from . import ingredients, dishes, orders
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(ingredients.router)
 router.include_router(dishes.router)
 router.include_router(orders.router)
-router.include_router(routes.router)
 
 
-del ingredients, dishes, orders, routes
+del ingredients, dishes, orders
